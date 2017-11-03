56eb22dd457bc1a20c130cd1fa6b52bc7ef38504
Benjamin Morel <benjamin.morel@gmail.com>
2017-11-03 18:30:11 +0100

    Use strict_types

--- src/Io/FileSystem.php
+++ src/Io/FileSystem.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Brick\Std\Io;
 
 use Brick\Std\ErrorCatcher;
--- src/Json/Common.php
+++ src/Json/Common.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Brick\Std\Json;
 
 use Brick\Std\ErrorCatcher;
